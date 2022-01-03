1176dd0b81f91812dd2d82d36261feff21675db3
Sam <sgulinello12@gmail.com>
2022-01-02 20:08:46 -0500

    organized file structure

--- Problem1.cpp
+++ Problem1.cpp
@@ -4,12 +4,22 @@ of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is
 23. Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-int sum() {
+#include <iostream>
+using namespace std;
 
-    int n = 10;
+int main() {
 
-    for(int i = 0; i <= 10; i++) {
-    
+    int n = 1000;
+    int sum = 0;
+
+    for(int i = 1; i <= n-1; i++) {
+        if(i % 3 == 0 || i % 5 ==0)
+        {
+            sum += i;
+        }
     }
 
+    cout << sum;
+
+    return 0;
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+The goal is to work on and hopefully solve one 'Project Euler' problem each day of 2022 using C++
+This is to gain a better understaning of and practice using the programming language as well as push my mind to solve complex issues logically with code.
+
+The link can be found [here](https://projecteuler.net/archives)
